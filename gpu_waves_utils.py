Partially fixed scaling problems. Wave length still seems to be broken.

gpu_waves_utils.py
@@ -61,7 +61,8 @@ def gpu_waves_step_emitters(heights: np.ndarray,
     x, y = cuda.grid(2)
     for e_x, e_y, wavelength, amplitude, offset in emitters:
         if e_x == x and e_y == y:
-            heights[x, y] = np.sin(offset + simulation_time / wavelength * pixels_per_centimeter * speed) * amplitude
+            heights[x, y] = np.sin((offset + simulation_time / wavelength / pixels_per_centimeter * speed)*np.pi*2)\
+                            * amplitude
 
 
 if __name__ == '__main__':