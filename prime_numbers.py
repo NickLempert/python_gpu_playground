1c9d8cea40b9655b1435d619a8318dc7b96e697d
NickLempert <nick209s21@gmail.com>
2026-07-10 13:16:02 +0300

    prime_numbers.py is now compare GPU to CPU.

--- prime_numbers.py
+++ prime_numbers.py
@@ -6,7 +6,7 @@ import numpy as np
 from utils import gpu_program
 
 
-@gpu_program(['void(int64[:])'], dimensions=1)
+@gpu_program(['void(uint64[:])'], dimensions=1)
 def get_primes(output: np.array):
     pos = cuda.grid(1)
     if pos < output.size:
@@ -19,11 +19,18 @@ def get_primes(output: np.array):
             a -= 1
 
 
-@gpu_program(['void(int64, int64[:])'], dimensions=1)
+@gpu_program(['void(uint64, uint64[:])'], dimensions=1)
 def is_prime(inp: int, output: np.array):
     pos = cuda.grid(1)
     if 1 < pos < inp and inp % pos == 0:
         output[0] = 0
+
+
+def non_gpu_is_prime(inp: int):
+    for num in range(2, int(inp**0.5)):
+        if inp % num == 0:
+            return False
+    return True
 
 
 if __name__ == '__main__':
@@ -32,11 +39,19 @@ if __name__ == '__main__':
     # for i in range(len(arr)):
     #     if arr[i]:
     #         print(i)
-    start_at = 100000000000000000000
-    search_count = 10000
+    start_at = 100000000000000
+    search_count = 1000
+    t = time.perf_counter()
     for i in range(start_at, start_at+search_count):
         arr = np.array([1])
         is_prime(i, arr, shape=[max(1000, int(i ** 0.5))])
-        if arr[0]:
-            print(i)
+        # if arr[0]:
+        #     print(i)
+    print(time.perf_counter()-t)
 
+    t = time.perf_counter()
+    for i in range(start_at, start_at+search_count):
+        test_out = non_gpu_is_prime(i)
+        # if test_out:
+        #     print(i)
+    print(time.perf_counter()-t)
